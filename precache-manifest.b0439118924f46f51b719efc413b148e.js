d9be543d4b1d8c2750d2c177cd5d833ac9134724
John Paul Andersen <neutronstar@gmx.us>
2019-02-08 23:27:44 -0700

    Updates

---
+++ precache-manifest.b0439118924f46f51b719efc413b148e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3407b471252f0044dc9",
+    "url": "/cs356-desktop/static/css/main.0972c7bc.chunk.css"
+  },
+  {
+    "revision": "b3407b471252f0044dc9",
+    "url": "/cs356-desktop/static/js/main.b3407b47.chunk.js"
+  },
+  {
+    "revision": "c37f4f6128cad892659a",
+    "url": "/cs356-desktop/static/css/1.e5fa8bf1.chunk.css"
+  },
+  {
+    "revision": "c37f4f6128cad892659a",
+    "url": "/cs356-desktop/static/js/1.c37f4f61.chunk.js"
+  },
+  {
+    "revision": "6d00649a3a09e30476b2",
+    "url": "/cs356-desktop/static/js/runtime~main.6d00649a.js"
+  },
+  {
+    "revision": "c76fd7bad885f278c357c281048c9c1c",
+    "url": "/cs356-desktop/static/media/photo.c76fd7ba.png"
+  },
+  {
+    "revision": "c8833c04e4e7f01c7cc4ce6fba8069f3",
+    "url": "/cs356-desktop/index.html"
+  }
+];
