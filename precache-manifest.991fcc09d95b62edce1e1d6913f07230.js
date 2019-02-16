98e0ed0db94daf5b79ef10b4b950447458d9729e
John Paul Andersen <neutronstar@gmx.us>
2019-02-15 21:09:49 -0700

    Updates

--- precache-manifest.991fcc09d95b62edce1e1d6913f07230.js
+++ precache-manifest.991fcc09d95b62edce1e1d6913f07230.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "44853720b15fbd1eeae7",
+    "revision": "376a3208d8cf2738e590",
     "url": "/cs356-desktop/static/css/main.7e3f24dc.chunk.css"
   },
   {
-    "revision": "44853720b15fbd1eeae7",
-    "url": "/cs356-desktop/static/js/main.44853720.chunk.js"
+    "revision": "376a3208d8cf2738e590",
+    "url": "/cs356-desktop/static/js/main.376a3208.chunk.js"
   },
   {
-    "revision": "96e52b6bb4b98bc369b7",
+    "revision": "38a54ed525ffbd6baca3",
     "url": "/cs356-desktop/static/css/1.f23841b3.chunk.css"
   },
   {
-    "revision": "96e52b6bb4b98bc369b7",
-    "url": "/cs356-desktop/static/js/1.96e52b6b.chunk.js"
+    "revision": "38a54ed525ffbd6baca3",
+    "url": "/cs356-desktop/static/js/1.38a54ed5.chunk.js"
   },
   {
     "revision": "6d00649a3a09e30476b2",
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/cs356-desktop/static/media/photo.c76fd7ba.png"
   },
   {
-    "revision": "9f4c36e47e139218354341bd0f5c9f9e",
+    "revision": "fad7bd8fb2fa7da090c6973ab858ce9f",
     "url": "/cs356-desktop/index.html"
   }
 ];
